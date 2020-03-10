b844935396bff5983de6cc0450389a44b9ad7203
Raajit Raj <raajit.raj@gmail.com>
2020-03-10 16:30:10 +0530

    generating proper name for the audio file

--- views/download.py
+++ views/download.py
@@ -2,10 +2,13 @@ import os
 
 from flask import Blueprint, current_app, send_file
 
+from ..models import Songs
 
 bp = Blueprint('download', __name__, url_prefix='/download')
 
 @bp.route('/<slug>', methods=["GET"])
 def download_file(slug):
     audio_file = os.path.join(current_app.config['UPLOAD_FOLDER'], slug)
-    return send_file(audio_file, as_attachment=True, attachment_filename=slug+".mp3")
+    song = Songs.get_song_from_slug(slug)
+    filename = f"{song.title.title()} - {song.album.title()}.mp3"
+    return send_file(audio_file, as_attachment=True, attachment_filename=filename)
