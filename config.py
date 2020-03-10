d0c6b849d6508e72da4d761d917d9ca518069d8c
Raajit Raj <raajit.raj@gmail.com>
2020-03-11 02:03:17 +0530

    getting the secret from the env variable

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+# TODO: Change this to different environments
+# Currently the config is setup for development environment
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db/songs.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
 WTF_CSRF_ENABLED = True
+SECRET_KEY = os.getenv('SECRET_KEY')
 UPLOAD_FOLDER = './media'
-SQLALCHEMY_ECHO=True
+SQLALCHEMY_ECHO = False
