db0a7099ef576f34845a7332b1ab8138ce1f5490
Raajit Raj <raajit.raj@gmail.com>
2020-03-11 02:01:09 +0530

    created a cli command to create UPLOADS_FOLDER

--- manage.py
+++ manage.py
@@ -3,7 +3,16 @@ from flask import Blueprint, cli, current_app
 
 from .models import db
 
+managecli = Blueprint('manage', __name__)
 dbcli = Blueprint('db', __name__)
+
+@managecli.cli.command("init")
+@cli.with_appcontext
+def init():
+    """Creates media/upload folder"""
+    media_folder = current_app.config['UPLOAD_FOLDER']
+    if not os.path.exists(media_folder):
+        os.mkdir(media_folder)
 
 @dbcli.cli.command("init")
 @cli.with_appcontext
@@ -14,4 +23,4 @@ def init_db():
         db_path = db_uri[len('sqlite:///'):]
     if not os.path.exists(db_path):
         os.system(f'mkdir db && touch {db_path}')
-    db.create_all()
+        db.create_all()
